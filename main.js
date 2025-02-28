1b51005ee8bef5548c1982a1d9ab78f3e9a26352
guiH0l1 <guilhermeholi09@gmail.com>
2025-02-28 19:17:18 -0300

    mongodb cadastro lanche, fazer o buscar

--- main.js
+++ main.js
@@ -23,10 +23,64 @@ const criarLanche = async (nomeLan, descLan, precoLan) => {
     }
 }
 
+const listarLanche = async () => {
+    try {
+        // a linha abaixo lista todos os clientes cadastrados em ordem alfabética
+        const lanche = await lanchesModel.find().sort(
+            {
+                nomeLanche: 1
+            }
+        )
+        console.log(lanche)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const buscarLanche = async (nome) => {
+    try {
+        // find() -> buscar 
+        // nomeCliente: new RegExp(nome) filtro pelo nome (partes que contenham (expressão regular))
+        // 'i' insentive (ignorar letras maiusculas e minusculas)
+        const lanche = await lanchesModel.find({ nomeLanche: new RegExp(nome, 'i') }
+        )
+        //calcular a similaridade entre os nomes retornados e o nome pesquisado
+        const nomeLanche = lanche.map(lanche => lanche.nomeLanche)
+
+        if (nomeLanche.length === 0) {
+            console.log("Lanche não cadastrado")
+        } else {
+            const match = stringSimilarity.findBestMatch(nome,
+                nomeLanche)
+            //lanche com melhor similaridade
+            const melhorLanche = lanche.find(lanche => lanche.nomeLanche === match.bestMatch.target)
+            //formatação da data
+            const lancheFormatado = {
+                nomeLanche: melhorLanche.nomeLanche,
+                descricaoLanche: melhorLanche.descricaoLanche,
+                preco: melhorLanche.preco,
+                dataCadastro: melhorLanche.dataCadastro.toLocaleDateString('pt-BR')
+            }
+            console.log(lancheFormatado)
+            //console.log(melhorCliente)
+        }
+
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
 const app = async () => {
     await conectar()
-    await criarLanche('Big Mac', '4 hamb, alface, cebola, tomate e pão', 'R$20,00')
+    //await criarLanche('cachorro-quente', 'pão, salsicha, molho de tomate, ketchup, mostarda, pure', 'R$15,00')
+    //await listarLanche()
+    await buscarLanche("heddar Bacon")
     await desconectar()
+    
 
 }
 
